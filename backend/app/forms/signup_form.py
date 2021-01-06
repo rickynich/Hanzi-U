b5d06244d1a88bd480d3714a8479a92eaf6fde6c
Nick Richard <nrich414@gmail.com>
2021-01-06 12:49:18 -0500

    work save. routes, forms, models from pystarter done

--- backend/app/forms/signup_form.py
+++ backend/app/forms/signup_form.py
@@ -3,14 +3,12 @@ from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
-
 def user_exists(form, field):
-    print("Checking if user exits", field.data)
+    print("Checking if user exists", field.data)
     email = field.data
     user = User.query.filter(User.email == email).first()
     if user:
-        raise ValidationError("User is already registered.")
-
+        raise ValidationError("User is already registered")
 
 class SignUpForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
