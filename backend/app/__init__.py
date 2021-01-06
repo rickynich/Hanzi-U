beb5d2bb01e3d96acf4a1c37449f442fa727b98e
Nick Richard <nrich414@gmail.com>
2021-01-06 11:02:24 -0500

    routes and dbs setup save

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,8 +3,5 @@ from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-print("HEEEELLLLLO")
 
-@app.route('/')
-def index():
-    return '<h1>Hanzi U<h1>'
+from app import routes
