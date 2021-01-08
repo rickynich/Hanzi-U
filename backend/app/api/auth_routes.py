3fc20cbe5b94380e2a83ac868dd64ae196f1b02b
Nick Richard <nrich414@gmail.com>
2021-01-07 19:58:27 -0500

    set password hash works

--- backend/app/api/auth_routes.py
+++ backend/app/api/auth_routes.py
@@ -3,6 +3,7 @@ from app.models import User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash
 
 auth_routes = Blueprint('auth', __name__)
 
@@ -61,7 +62,7 @@ def sign_up():
         user = User(
             username=form.data['username'],
             email=form.data['email'],
-            hashed_password=form.data['password']
+            password=form.data['password']
         )
         db.session.add(user)
         db.session.commit()
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -9,31 +9,34 @@ class User(db.Model, UserMixin):
   username = db.Column(db.String(40), nullable= False, unique= True)
   email = db.Column(db.String(255), nullable = False, unique = True)
   hashed_password = db.Column(db.String(255), nullable = False) #changed
-  exp = db.Column(db.Integer, nullable = True)
+  exp = db.Column(db.Integer, nullable = True, default=0)
 
   decks = db.relationship("Deck", back_populates="user")
 
 
-@property
-def password(self):
-  return self.hashed_password
+  @property
+  def password(self):
+    return self.hashed_password
 
 
-@password.setter
-def password(self, password):
-  self.hashed_password = generate_password_hash(password)
+  @password.setter
+  def password(self, password):
+    print("In password setter***********", password)
+    self.hashed_password = generate_password_hash(password)
+    print("***********In password setter", self.hashed_password)
 
 
-def check_password(self, password):
-  return check_password_hash(self.password, password)
+  def check_password(self, password):
+    return check_password_hash(self.password, password)
 
-def to_dict(self):
-  return {
-    "id": self.id,
-    "username": self.username,
-    "email": self.email,
-    "exp": self.exp
-  }
+
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "username": self.username,
+      "email": self.email,
+      "exp": self.exp
+    }
 
 # def __repr__(self):
 #   return '<User>{}'.format(self.id)
