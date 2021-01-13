df717817ca4e261ed8fccc5744f6ce856d1c11a0
Nick Richard <nrich414@gmail.com>
2021-01-12 22:21:31 -0500

    card component work

--- react-app/src/components/Card.js
+++ react-app/src/components/Card.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { ThemeProvider, Box } from "@chakra-ui/react";
+import { ThemeProvider, Box, Text } from "@chakra-ui/react";
 
 // antique white: #FAEBD7
 function Card(props) {
-	console.log("Card props", props)
+	console.log("Card props", props);
 	return (
-		<>
-			<Box w="200px" h="15vh" boxShadow="lg" rounded="md">
-				<p>{props.character.character}</p>
-				<p>{props.character.definition}</p>
+		<div display="flex">
+			<Box w="400px" h="15vh" boxShadow="lg" rounded="md" bg="gray.200">
+				<Box>{props.character.character}</Box>
+				<Box>
+					<Text>{props.character.definition}</Text>
+				</Box>
 			</Box>
-		</>
+		</div>
 	);
 }
 
