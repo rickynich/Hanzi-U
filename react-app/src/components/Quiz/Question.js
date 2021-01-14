362b62a922e1368ab271a25e93ec7541b6df3f1f
Nick Richard <nrich414@gmail.com>
2021-01-14 12:30:17 -0500

    deck ramdomizer for quiz, pass to Question component

--- react-app/src/components/Quiz/Question.js
+++ react-app/src/components/Quiz/Question.js
@@ -3,10 +3,11 @@ import { useDeck } from "../Context/DeckContext";
 //custom components:
 import Answer from "./AnswerChoice";
 
-function Question() {
+function Question(props) {
+      console.log("Question Component - questionDeck", props.questionDeck.characters[0].character)
 	return (
 		<>
-            <h1>A question(pinyin and definition) will be rendered here</h1>
+                  <h1>{props.questionDeck.characters[0].character}</h1>
             <Answer>Answer Choice 1 (A character)</Answer>
             <Answer>Answer Choice 2 (A character)</Answer>
             <Answer>Answer Choice 3 (A character)</Answer>
--- react-app/src/components/Quiz/Quiz.js
+++ react-app/src/components/Quiz/Quiz.js
@@ -16,38 +16,61 @@ import Question from "./Question";
 function Quiz() {
 	const { deckId } = useParams();
 	const decks = useDeck(); //uses DeckContext
-    
+
 	const [deck, setDeck] = useState(decks[deckId - 1]);
-	const [quizCharacters, setQuizCharacters] = useState([]); //each question will have 3 characters as possible answers, and one pinyin prompt (the q)
+	const [questionDeck, setQuestionDeck] = useState([]); //each question will have 3 characters as possible answers, and one pinyin prompt (the q)
 	const [score, useScore] = useState(0);
 	const [answerChoices, setAnswerChoices] = useState([]);
 	const [characterQuestion, setCharacterQuestion] = useState({});
-    
-    if (!deck) return null;
+	
+	if (!deck) return null;
 	// question will be the {character} and the [randomized answers]
 	// useEffect(() => {
 	//     const shuffledAnswerOptions = quizCharacters.map((character) => this.shuffleArray(question.answers))
 	//     setCharacterQuestion(quizQuestions[0])
 	// })
+	let array = deck.characters
 
 	const shuffleArray = (array) => {
-		let currentIndex = array.length,
-			tempVal,
-			randomIndex;
-	};
+		let currentIndex = array.length;
+		let tempVal;
+		let randomIndex;
+		console.log("array going in ", array)
+		let randomizedArray = []
 
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			console.log("randomIndex", randomIndex);
+			console.log("currentIndex", currentIndex);
+			// And swap it with the current element.
+			tempVal = array[currentIndex];
+			console.log("Current Index at array to set to tempVal", tempVal)
+			array[currentIndex] = array[randomIndex];
+			// randomizedArray[currentIndex] = array[randomIndex]
+			// randomizedArray[randomIndex] = tempVal;
+			// setQuestionDeck(array[randomIndex])
+			console.log("(array[currentIndex]) = array[randomIndex]  ", array[randomIndex]);
+			array[randomIndex] = tempVal;
+		}
+		console.log("Array after shuffle", array)
+		// console.log("randomized after loop", randomizedArray)
+		return array;
+	};
+	shuffleArray(array)
+	console.log("Quesiton deck: ",questionDeck)
 	console.log("deck selected in quiz", deck);
 
 	// const getQuestions = () => {
 
 	// }
 	return (
-		(
-			<>
-				<h1>Welcome to the {deck.name} quiz, time to start!</h1>
-				<Question>A question will appear</Question>
-			</>
-		)
+		<>
+			<h1>Welcome to the {deck.name} quiz, time to start!</h1>
+			<Question questionDeck={deck}>A question will appear</Question>
+		</>
 	);
 }
 
