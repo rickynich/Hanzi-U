1bfaae14b01d246206912c07483f80437342786c
Nick Richard <nrich414@gmail.com>
2021-01-14 21:31:25 -0500

    RadioCard component made for answer choices, eod sign off

--- react-app/src/components/Quiz/AnswerChoices.js
+++ react-app/src/components/Quiz/AnswerChoices.js
@@ -1,12 +1,24 @@
-import { Radio, RadioGroup, Stack } from "@chakra-ui/react";
+import {
+	HStack,
+	Radio,
+	RadioGroup,
+	Stack,
+	useRadioGroup,
+} from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import { useDeck } from "../Context/DeckContext";
-
+import RadioCard from "./RadioCard";
 
 function Answers(props) {
 	const [value, setValue] = useState("1");
-	let {choices} = props
-	console.log("Choices in answer Choices", choices)
+	let { choices } = props;
+
+	const { getRootProps, getRadioProps } = useRadioGroup({
+		name: "framework",
+		defaultValue: "react",
+		onChange: console.log,
+	});
+	console.log("Choices in answer Choices", choices);
 
 	const shuffleArray = (array) => {
 		let currentIndex = array.length;
@@ -23,18 +35,32 @@ function Answers(props) {
 	};
 	shuffleArray(choices);
 
+	const group = getRootProps();
 	return (
-		<>
-			<RadioGroup onChange={() => setValue(value)} value={value}>
-				<Stack direction="row">
-					<Radio value="1">Answer Choice 1 {choices[0].character}</Radio>
-					<Radio value="2">Answer Choice 2 {choices[1].character}</Radio>
-					<Radio value="3">Answer Choice 3 {choices[2].character}</Radio>
-					{/* <Radio value="4">Answer Choice 4 {choices[3].character}</Radio> */}
-				</Stack>
-			</RadioGroup>
-		</>
+		<HStack {...group}>
+			{choices.map((value) => {
+				const radio = getRadioProps({ value });
+				return (
+					<RadioCard key={value} {...radio}>
+						{value}
+					</RadioCard>
+				);
+			})}
+		</HStack>
 	);
 }
 
 export default Answers;
+
+// return (
+// 	<>
+// 		<RadioGroup defaultValue="1">
+// 			<Stack spacing={4} direction="row">
+// 				<Radio value="1">Answer Choice 1 {choices[0].character}</Radio>
+// 				<Radio value="2">Answer Choice 2 {choices[1].character}</Radio>
+// 				<Radio value="3">Answer Choice 3 {choices[2].character}</Radio>
+// 				{/* <Radio value="4">Answer Choice 4 {choices[3].character}</Radio> */}
+// 			</Stack>
+// 		</RadioGroup>
+// 	</>
+// );
---
+++ react-app/src/components/Quiz/RadioCard.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { ThemeProvider, Box, Text, useRadio } from "@chakra-ui/react";
+
+// antique white: #FAEBD7
+function RadioCard(props) {
+	const { getInputProps, getCheckboxProps } = useRadio(props);
+
+	const input = getInputProps();
+	const checkbox = getCheckboxProps();
+	return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderWidth="1px"
+        borderRadius="md"
+        boxShadow="md"
+        _checked={{
+          bg: "teal.600",
+          color: "white",
+          borderColor: "teal.600",
+        }}
+        _focus={{
+          boxShadow: "outline",
+        }}
+        px={5}
+        py={3}
+      >
+        {props.children}
+      </Box>
+    </Box>
+  )
+}
+
+
+export default RadioCard;
+
+// return (
+// 	<Box>
+// 		<Box w="300px" h="30vh" boxShadow="lg" rounded="md" bg="gray.200">
+// 			<Box bg="#FAEBD7">
+// 				<Text>Character: {props.character.character}</Text>
+// 			</Box>
+// 			<Box h="15vh">
+// 				<Text>Pinyin: {props.character.pinyin}</Text>
+// 				<Text>Definition: {props.character.definition}</Text>
+// 				<Text>Hint/reminder: {props.character.hint}</Text>
+// 			</Box>
+// 		</Box>
+// 	</Box>
+// );
