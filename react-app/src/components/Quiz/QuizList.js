d7cf62c15c05cab44c4cbc5cd2e47a70a1dbda50
Nick Richard <nrich414@gmail.com>
2021-01-18 20:58:56 -0500

    Quiz list styled

--- react-app/src/components/Quiz/QuizList.js
+++ react-app/src/components/Quiz/QuizList.js
@@ -3,7 +3,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 //Chakra
-import { Button } from "@chakra-ui/react";
+import { Button, Flex, Heading, HStack } from "@chakra-ui/react";
 import { useDeck } from "../Context/DeckContext";
 
 //context:
@@ -20,19 +20,21 @@ function QuizPage() {
 	if (decks.length == 0) return null;
 	const deckComponents = decks.map((deck) => {
 		return (
-			<li key={deck.id}>
+			<>
 				<Button onClick={() => handleClick(deck.id)} id={deck.id}>
 					{deck.name}
 				</Button>
-			</li>
+			</>
 		);
 	});
 
 	return (
-		<>
-			<h1>Deck List: </h1>
-			<ul>{deckComponents}</ul>
-		</>
+		<Flex direction="column">
+			<Heading size="lg" mb={3} mr={3}>
+				Deck List:{" "}
+			</Heading>
+			<HStack mb={6}>{deckComponents}</HStack>
+		</Flex>
 	);
 }
 export default QuizPage;
